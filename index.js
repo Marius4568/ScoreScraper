c074ac42ef41b151e174ae32706ac9ac9916f170
Arturianas <arturasdomnenko@gmail.com>
2023-04-11 22:32:39 +0300

    04-11 score scrapper

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require('fs')
 
-const scrapeNBAScores = async () => {
+const scrapeNBAScores = async (date) => {
   try {
-    const response = await axios.get("https://www.espn.com/nba/scoreboard");
+    const response = await axios.get(`https://www.espn.com/nba/scoreboard/_/date/${date}`);
 
     if (response.status !== 200) {
       console.error("Error: Unable to fetch data from ESPN.");
@@ -13,7 +14,7 @@ const scrapeNBAScores = async () => {
     const $ = cheerio.load(response.data);
     const games = [];
 
-    $(".ScoreCell__Team").each((index, element) => {
+    $(".ScoreboardScoreCell__Competitors").each((index, element) => {
       const teams = [];
       const scores = [];
 
@@ -26,8 +27,7 @@ const scrapeNBAScores = async () => {
       $(element)
         .find(".ScoreCell__Score")
         .each((i, scoreElem) => {
-        //   scores.push(parseInt($(scoreElem).text().trim()));
-        scores.push($(scoreElem).text.trim());
+        scores.push($(scoreElem).text());
         });
 
       games.push({
@@ -41,12 +41,54 @@ const scrapeNBAScores = async () => {
         },
       });
     });
-
-    // console.log(games);
-    console.log(games);
+    return games;
   } catch (error) {
     console.error("Error: Unable to fetch data from ESPN.", error);
   }
 };
 
-scrapeNBAScores();
+const allDayScores = [];
+
+const forLoop = async _ => {
+  console.log('Start')
+  const days = [1,2,3,4,5,6,7,8,9,10,11];
+  const allDayScores = await Promise.all(days.map(async (day) => {
+    const date = `202304${String(day).padStart(2, '0')}`;
+    const result = await scrapeNBAScores(date);
+    return {
+      day,
+      result
+    };
+  }));
+  console.log(allDayScores[1].result[0])
+  console.log('End')
+}
+
+forLoop();
+
+
+
+
+
+
+
+
+
+// const asyncCall = async (item) => {
+//   // Your async function logic here
+//   console.log(`Async call for item: ${item}`);
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(`Processed item: ${item}`);
+//     }, 1000);
+//   });
+// };
+
+// async function processItems() {
+//   const items = Array.from({ length: 10 }, (_, i) => i + 1);
+//   const results = await Promise.all(items.map(async (item) => await asyncCall(item)));
+//   console.log('All async calls are completed:', results);
+// }
+
+// processItems();
+
