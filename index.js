063fa58dcb6f00f5dd548cbf08faa61541b14150
Arturianas <arturasdomnenko@gmail.com>
2023-04-16 12:36:46 +0300

    add cors origin

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const cors = require("cors");
 const scrapeRoute = require("./routes/scrapeRoute")
 
 const app = express();
+
+const corsOptions = {
+    origin: 'https://scorescraper-production.up.railway.app/', // Replace with your actual Vercel domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  };
+
 app.use(cors())
 app.use("/scrape", scrapeRoute);
 
