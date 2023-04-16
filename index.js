557d340e685ab668a349dc064c0748be784f9bf5
Arturianas <arturasdomnenko@gmail.com>
2023-04-16 12:49:32 +0300

    add dotenv config

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const scrapeRoute = require("./routes/scrapeRoute")
+const { Port } = require('./config');
 
 const app = express();
-
-const corsOptions = {
-    origin: 'https://scorescraper-production.up.railway.app/', // Replace with your actual Vercel domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  };
 
 app.use(cors())
 app.use("/scrape", scrapeRoute);
 
-const PORT = 4000;
-app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+app.listen(Port, () => console.log(`server is running on ${Port}`))
